022df370005533802e8f991df98882bcb4244965
arthuradriaens-code <arthur99.adriaens@gmail.com>
2022-11-27 00:41:04 +0100

    I think the fractal structure looks ok, also done some sims

--- project/GrandSearch/32/LazyScript.sh
+++ project/GrandSearch/32/LazyScript.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-# run
-cd "/home/arthur/Universiteit/CompMatPhys/Fe-Si/project/GrandSearch/32/2"
-mpirun --use-hwthread-cpus pw.x < 2Si_1.in > 2Si_1.out && notify-send "2 Si nr 1 jobs done 🦆"
+#run
+cd "/home/arthur/Universiteit/CompMatPhys/Fe-Si/project/GrandSearch/32/4"
+notify-send "Starting 4 Si job 🦆"
+mpirun --use-hwthread-cpus pw.x < 5Si.in > 5Si.out && notify-send "4 Si jobs done 🦝"
 # cleanup
 rm Fe.pbe-spn-kjpaw_psl.0.2.1.UPF
 rm Si.pbe-n-rrkjus_psl.1.0.0.UPF
+rm -rf file.*
 
 #run
-cd "/home/arthur/Universiteit/CompMatPhys/Fe-Si/project/GrandSearch/32/5"
-mpirun --use-hwthread-cpus pw.x < 5Si.in > 5Si.out && notify-send "5 Si jobs done 🦝"
+cd "/home/arthur/Universiteit/CompMatPhys/Fe-Si/project/GrandSearch/32/6"
+notify-send "Starting 6 Si job 🦆"
+mpirun --use-hwthread-cpus pw.x < 5Si.in > 5Si.out && notify-send "6 Si jobs done 🦝"
 # cleanup
 rm Fe.pbe-spn-kjpaw_psl.0.2.1.UPF
 rm Si.pbe-n-rrkjus_psl.1.0.0.UPF
-
-
+rm -rf file.*
